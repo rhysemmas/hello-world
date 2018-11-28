9ec68ac565533e7aeedd2601d00efa06ba909dee
Rhys Emmas <rhys.emmas@ntlworld.com>
2018-11-28 07:57:43 +0000

    added command to prune dangling intermediary images

--- scripts/rm-d.sh
+++ scripts/rm-d.sh
@@ -1,1 +1,1 @@
-docker ps -a | awk '{ print $1,$2 }' | grep hello-world | awk '{print $1 }' | xargs -I {} sh -c 'docker kill {}; docker rm {};'
+docker ps -a | awk '{ print $1,$2 }' | grep hello-world | awk '{print $1 }' | xargs -I {} sh -c 'docker kill {}; docker rm {};' && docker image prune -f
